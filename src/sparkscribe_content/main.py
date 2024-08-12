566baed40bb8ef22db1b0bb6c6dff41bade7bbd7
Zahid Hasan <75244912+z4hid@users.noreply.github.com>
2024-08-12 20:12:17 +0600

    Update main.py

--- src/sparkscribe_content/main.py
+++ src/sparkscribe_content/main.py
@@ -5,6 +5,6 @@ from sparkscribe_content.crew import SparkscribeContentCrew
 def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
-        'topic': 'AI LLMs'
+        'topic': 'Machine Learning'
     }
     SparkscribeContentCrew().crew().kickoff(inputs=inputs)
